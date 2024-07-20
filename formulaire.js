11b1d39a6578e9c4f88c053aa071bf49317bfbab
MBass5 <montaolivier@gmail.com>
2024-07-20 17:25:46 +0200

    Pages accueil+contacts

---
+++ formulaire.js
@@ -0,0 +1,20 @@
+function imprimer() {
+  // @ts-ignore
+  const firstName = document.getElementById("firstname").value;
+  console.log(firstName);
+  // @ts-ignore
+  const name = document.getElementById("name").value;
+  console.log(name);
+  // @ts-ignore
+  const societe = document.getElementById("societe").value;
+  console.log(societe);
+  // @ts-ignore
+  const message = document.getElementById("messageArea").value;
+  console.log(message);
+  // @ts-ignore
+  const particulier = document.getElementById("particulier").value;
+  console.log(particulier);
+  // @ts-ignore
+  const professionnel = document.getElementById("professionnel").value;
+  console.log(professionnel);
+}
